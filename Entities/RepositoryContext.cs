b7d661a681393afc274da76ba03d97bdbbc9be0a
John Sourvinos <johnsourvinos@hotmail.com>
2022-09-07 12:30:19 +0300

    Adds RepositoryBase class

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -6,6 +6,11 @@ namespace Entities {
 
         public RepositoryContext(DbContextOptions options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
+        
         public DbSet<Company> Companies { get; set; }
         public DbSet<Employee> Employees { get; set; }
 
